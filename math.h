26942c576d9c20287bf47d70f86a8302049bbb30
denismuh2 <denismuh20@gmail.com>
2026-04-02 13:00:14 +0700

    Upd .h

--- math.h
+++ math.h
@@ -1,7 +1,16 @@
 #ifndef MATH_H
 #define MATH_H
 
+// проверка на сущ треугольника
+bool isTriangle(double a,double , double c);
+
+// проверка на равнобедренность 
+bool isTriangleRB(double a, double b, double c);
+
+// вычисление периметра
 double perimeter(double a, double b, double c);
+
+// вычисление площади по формуле Герона
 double space(double a, double b, double c);
 
 #endif // MATH_H
